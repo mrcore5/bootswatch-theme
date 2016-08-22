e1c2164931b447645db58a25e09b917a57a694cb
Matthew Reschke <mail@mreschke.com>
2016-08-22 10:54:37 -0600

    Move app building out of wiki

--- Http/routes.php
+++ Http/routes.php
@@ -1,10 +1,1 @@
 <?php
-
-Route::get('/', function() {
-	return view('welcome-to-mrcore');
-});
-
-
-Route::get('/grid', function() {
-	return view('grid');
-});
--- gulpfile.js
+++ gulpfile.js
@@ -14,39 +14,30 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 
-	// Compile less styles
-	mix.less('../../../Styles/app.less', 'Assets/css/theme-bundle.css');
+	mix.styles([
+		"font-awesome-animation.min.css",
+		"font-awesome.min.css",
+		"yamm.css",
+		"jquery.growl.css",
+		"dataTables.bootstrap.css"
+	], 'Assets/css/theme-bundle.css', 'Assets/css');
 
-	// Copy font awesome fonts
-	mix.copy('node_modules/font-awesome/fonts', 'Assets/fonts');
-
-	// Compile javascript
-	mix.browserify('app.js', 'Assets/js/theme-bundle.js', 'Scripts');
-
-	/*mix.styles([
-		-"font-awesome-animation.min.css",
-		-"font-awesome.min.css",
-		-"yamm.css",
-		-"jquery.growl.css",
-		-"dataTables.bootstrap.css"
-	], 'Assets/css/theme-bundle.css', 'Styles/vendor');
-	*/
-
-	/*mix.scripts([
-		-"jquery-2.1.4.min.js",
-		-"jquery.hotkeys.min.js",
-		-"jquery.dataTables.1.10.7.min.js",
-		-"jquery.dataTables.bootstrap.js",
-		-"-jquery.growl.js",
-		-"dataTables.js",
-		-"bootstrap.js",
-		-"vue.js"
-	], 'Assets/js/theme-bundle.js', 'Scripts/vendor');*/
+	mix.scripts([
+		"jquery-2.1.4.min.js",
+		"jquery.hotkeys.min.js",
+		"jquery.dataTables.1.10.7.min.js",
+		"jquery.dataTables.bootstrap.js",
+		"jquery.growl.js",
+		"dataTables.js",
+		"bootstrap.min.js",
+		"prism.js",
+		//"vue.js"
+	], 'Assets/js/theme-bundle.js', 'Assets/js');
 
 	mix.scripts([
 		"html5shiv.js",
 		"respond.min.js"
-	], 'Assets/js/shiv-bundle.js', 'Scripts/vendor');
+	], 'Assets/js/shiv-bundle.js', 'Assets/js');
 
 });
 
